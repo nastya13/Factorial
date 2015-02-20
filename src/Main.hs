afeb1d9214204259d81bbe90c0e1cd0dab695c90
nastya13 <nastya94-94.08@mail.ru>
2015-02-20 11:48:08 +0300

    Adding src-folder

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+
+--- factorial
+--- if < 0 then -1 "error"
+fac :: Int -> Int
+fac a = 
+  if a < 0
+    then - 1
+    else 
+    let
+      stop a = a < 1
+
+      go res a = if stop a
+	  	  then res
+		  else let res' = res * a
+			   a' = a - 1
+		       in go res' a'
+    in go 1 a
+main = print (fac 3)
